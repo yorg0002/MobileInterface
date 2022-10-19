706b4b6a65d15608c62ae2eca812d380e9c2c46d
Ece Selin Yorgancilar <yorg0002@algonquinlive.com>
2022-10-19 14:05:34 -0400

    Finished Week4

--- app/src/main/java/algonquin/cst2335/yorg0002/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/yorg0002/MainActivity.java
@@ -2,63 +2,86 @@ package algonquin.cst2335.yorg0002;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imgView;
+    Switch sw;
+    Button loginButton;
+    EditText emailEditText, passwordEditText;
+    String emailAddress;
 
 
 
     @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-        Intent nextPage = new Intent( MainActivity.this, SecondActivity.class);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
 
-        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
 
-        Button loginButton = findViewById(R.id.loginButton);
-        EditText EditText = findViewById(R.id.editTextTextPersonName);
-        loginButton.setOnClickListener( clk-> {
-            nextPage.putExtra( "EmailAddress", EditText.getText().toString() );
+        loginButton = findViewById(R.id.loginButton);
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        emailAddress = prefs.getString("LoginName", "");
+        emailEditText.setText(emailAddress);
+
+
+        Log.w("MainActivity", "In onCreate() - Loading Widgets");
+        loginButton.setOnClickListener(clk -> {
+            Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
+            SharedPreferences.Editor editor = prefs.edit();
+//            nextPage.putExtra("emailAddress", emailEditText.getText().toString());
+            nextPage.putExtra("password", passwordEditText.getText().toString());
+            editor.putString("LoginName", emailEditText.getText().toString());
+            editor.apply();
             startActivity(nextPage);
+        });
+    }
 
-        } );
-
-
-        }
     @Override
     protected void onStart() {
         super.onStart();
-        Log.w( "MainActivity", "In onStart() - The application is now visible" );
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.w( "MainActivity", "In onResume() - The application is now responding to user input" );
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.w( "MainActivity", "In onPause() - The application no longer responds to user input" );
+        Log.w("MainActivity", "In onStart() - Visible on screen");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.w( "MainActivity", "In onStop() - The application is no longer visible" );
+        Log.w("MainActivity", "In onStop() - No longer visible");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.w( "MainActivity", "In onDestroy() - Any memory used by the application is freed" );
+        Log.w("MainActivity", "In onDestroy() - Any memory used by the application is freed");
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w("MainActivity", "In onPause() - No longer responds to user input");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w("MainActivity", "In Resume() - Responding to user input");
+    }
+
 
 }
--- app/src/main/java/algonquin/cst2335/yorg0002/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/yorg0002/SecondActivity.java
@@ -4,117 +4,121 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
-import android.media.Image;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 
 public class SecondActivity extends AppCompatActivity {
-    ImageView Imageview;
+    TextView title;
+    Button callNumberButton, changePictureButton;
+    EditText editTextPhone;
+    ImageView profileImage;
+
+    String phoneNumber;
+
+    ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        Bitmap thumbnail = data.getParcelableExtra("data");
+//                        profileImage.setImageBitmap(thumbnail);
+
+                        FileOutputStream fOut = null;
+
+                        try {
+                            fOut = openFileOutput("Picture.png", Context.MODE_PRIVATE);
+                            thumbnail.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                            fOut.flush();
+                            fOut.close();
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_second);
-
         Intent fromPrevious = getIntent();
-        String emailAddress = fromPrevious.getStringExtra("EmailAddress");
-        TextView welcomeMessage = findViewById(R.id.welcomeView);
-        welcomeMessage.setText("Welcome " + emailAddress);
+//        String emailAddress = fromPrevious.getStringExtra("emailAddress");
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        String emailAddress = prefs.getString("LoginName", "");
+        String password = fromPrevious.getStringExtra("password");
+        phoneNumber = prefs.getString("PhoneNumber", "");
+        editTextPhone = findViewById(R.id.editTextPhone);
+        editTextPhone.setText(phoneNumber);
 
-        EditText EditText = findViewById(R.id.editTextTextPersonName);
-        EditText EditTextPhone = findViewById(R.id.editTextPhone);
+        title = findViewById(R.id.title);
+        title.setText("Welcome back " + emailAddress);
 
-        Intent call = new Intent(Intent.ACTION_DIAL);
-        call.setData(Uri.parse("tel:" + EditTextPhone));
+        callNumberButton = findViewById(R.id.callNumberButton);
+        callNumberButton.setOnClickListener(click -> {
+            editTextPhone = findViewById(R.id.editTextPhone);
 
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(clk -> {
-            String phone = fromPrevious.getStringExtra("PhoneNumber");
-
-            Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
-            EditText phoneNumberEdit = findViewById(R.id.editTextPhone);
-            call.setData(Uri.parse("tel:" + phoneNumberEdit.getText()));
-
+            phoneNumber = editTextPhone.getText().toString();
+            Intent call = new Intent(Intent.ACTION_DIAL);
+            call.setData(Uri.parse("tel:" + phoneNumber));
             startActivity(call);
-
-
         });
 
-        if (ContextCompat.checkSelfPermission(SecondActivity.this, Manifest.permission.CAMERA)
-            != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(SecondActivity.this, new String[] {
-                    Manifest.permission.CAMERA}, 100
-            );
-        }
-        Button changePicture = findViewById(R.id.button4);
-        this.Imageview = findViewById(R.id.avatar);
-        changePicture.setOnClickListener(click -> {
+        changePictureButton = findViewById(R.id.changePictureButton);
+        profileImage = findViewById(R.id.profileImage);
+
+        changePictureButton.setOnClickListener( click -> {
             Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(cameraIntent, 100);
-//            ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
-//                    new ActivityResultContracts.StartActivityForResult(),
-//                    new ActivityResultCallback<ActivityResult>() {
-//                        onActivityResult(cameraResult);
-//
-//                    });
-//            cameraResult.launch(cameraIntent);
+            cameraResult.launch(cameraIntent);
         });
 
+        File file = new File(getFilesDir(),"Picture.png" );
+        if (file.exists()) {
+            Bitmap theImage = BitmapFactory.decodeFile(getFilesDir() +"/Picture.png");
+            profileImage.setImageBitmap(theImage);
+        }
 
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editTextPhone = findViewById(R.id.editTextPhone);
+        editor.putString("PhoneNumber", editTextPhone.getText().toString());
+        editor.apply();
 
 
-        if (requestCode == 100) {
-            if (resultCode == Activity.RESULT_OK) {
-                Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
-                Imageview.setImageBitmap(thumbnail);
-
-                FileOutputStream fOut = null;
-
-                try { fOut = openFileOutput("Picture.png", Context.MODE_PRIVATE);
-
-                    thumbnail.compress(Bitmap.CompressFormat.PNG, 100, fOut);
-
-                    fOut.flush();
-
-                    fOut.close();
-
-                }
-
-                catch (FileNotFoundException e)
-
-                { e.printStackTrace();
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w("MainActivity", "In Resume() - Responding to user input");
+        File file = new File(getFilesDir(),"Picture.png" );
+        if (file.exists()) {
+            Bitmap theImage = BitmapFactory.decodeFile(getFilesDir() +"/Picture.png");
+            profileImage.setImageBitmap(theImage);
         }
-
     }
 }
